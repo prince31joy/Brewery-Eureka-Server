9ed6ba45215aa88046858e3944b628d539934151
joyp1 <joyp1@INITJOYP1L1C.apac.dell.com>
2020-08-11 16:19:23 +0530

    Initial Commit

---
+++ src/test/java/com/sfgbrewery/eureka/MsscBreweryEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sfgbrewery.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBreweryEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
